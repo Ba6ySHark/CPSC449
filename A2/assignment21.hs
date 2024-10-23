b75ed2f31551bc8f2c032f12d4c64534cc308cf0
Ba6ySHark <steven.susorov@gmail.com>
2024-10-22 19:58:00 -0700

    Added Assignment 2 template

---
+++ A2/assignment21.hs
@@ -0,0 +1,25 @@
+--Question 1
+transposeMatrix :: [[a]] -> [[a]]
+-- write your code here -- 
+
+--Question 2
+groupByPredicate :: (a -> Bool) -> [a] -> [[a]]
+-- write your code here --
+
+
+--Question 3
+data Movie = Movie { title :: String, rating :: Float, genre :: String } deriving Show
+
+topRatedMovies :: [Movie] -> Float -> [String]
+-- write your code for here --
+
+moviesByGenre :: [Movie] -> String -> [String]
+-- write your code here --
+
+adjustedRatings :: [Movie] -> [Movie]
+-- write your code here --
+
+
+--Question 4
+mergeSort :: Ord a => [a] -> [a]
+-- write your code here --
